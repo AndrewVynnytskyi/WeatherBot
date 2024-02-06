037316b86f4619859b84e926aa205461499f5055
AndrewVynnytskyi <nikoljavin@gmail.com>
2024-02-06 19:59:56 +0200

    Hosting add port

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+
 
 public class Main {
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException, IOException {
 
 
         TelegramBotsApi Weather = new TelegramBotsApi(DefaultBotSession.class);
         Weather.registerBot(new WeatherAbilityBot());
-
+        int port = Integer.parseInt(System.getenv("PORT"));
+        GreetServer server=new GreetServer();
+        server.start(port);
     }
 }
